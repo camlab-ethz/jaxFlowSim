1092158dae4828ee12ec3c6f126af1a8135940f9
DiegoRenner <diego.renner@hotmail.com>
2023-11-30 10:46:28 +0100

    added test cases from vascularmodel.com and cleaned up main_jax.py

--- main_jax.py
+++ main_jax.py
@@ -8,13 +8,22 @@ import os
 
 #jax.devices("gpu")[0]
 config.update("jax_enable_x64", True)
+
+# base cases
 #input_filename = "test/single-artery/single-artery.yml"
 #input_filename = "test/tapering/tapering.yml"
 #input_filename = "test/conjunction/conjunction.yml"
 #input_filename = "test/bifurcation/bifurcation.yml"
 #input_filename = "test/aspirator/aspirator.yml"
+
+# openBF-hub 
 input_filename = "test/adan56/adan56.yml"
-#conv_ceil = 0.001
+
+# vascularmodels.com
+#modelname = "0007_H_AO_H"
+#modelname = "0029_H_ABAO_H"
+#modelname = "0053_H_CERE_H"
+#input_filename = "test/" + modelname + "/" + modelname + ".yml"
 
 #config.update('jax_disable_jit', True)
 #with jax.checking_leaks():
