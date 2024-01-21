4dda6d5180a105b0b4ffd56ac258e9299565e75b
DiegoRenner <diego.renner@hotmail.com>
2024-01-21 20:22:11 +0100

    added script to plot bifurcation results for different wk params

---
+++ compare_output_params.py
@@ -0,0 +1,86 @@
+from src.model import configSimulation, simulationLoop
+import jax
+import sys
+import time
+import os
+from functools import partial
+from jax import block_until_ready, jit
+import matplotlib.pyplot as plt
+import shutil
+
+os.chdir(os.path.dirname(__file__))
+jax.config.update("jax_enable_x64", True)
+
+config_filename = "test/bifurcation/bifurcation.yml"
+(N, B, J, 
+ sim_dat, sim_dat_aux, sim_dat_const, sim_dat_const_aux, 
+ timepoints, conv_toll, Ccfl, edges, input_data, 
+            rho, total_time, nodes, 
+            starts, ends,
+            indices_1, indices_2,
+            vessel_names, cardiac_T) = configSimulation(config_filename)
+sim_loop_old_jit = partial(jit, static_argnums=(0, 1, 2))(simulationLoop)
+sim_dat1, t1, P1  = block_until_ready(sim_loop_old_jit(N, B, J, 
+                                      sim_dat, sim_dat_aux, sim_dat_const, sim_dat_const_aux, 
+                                      timepoints, conv_toll, Ccfl, edges, input_data, 
+                                      rho, total_time, nodes, 
+                                      starts, ends,
+                                      indices_1, indices_2))
+
+config_filename = "test/bifurcation2/bifurcation2.yml"
+(N, B, J, 
+ sim_dat, sim_dat_aux, sim_dat_const, sim_dat_const_aux, 
+ timepoints, conv_toll, Ccfl, edges, input_data, 
+            rho, total_time, nodes, 
+            starts, ends,
+            indices_1, indices_2,
+            vessel_names, cardiac_T) = configSimulation(config_filename)
+sim_loop_old_jit = partial(jit, static_argnums=(0, 1, 2))(simulationLoop)
+sim_dat2, t2, P2  = block_until_ready(sim_loop_old_jit(N, B, J, 
+                                      sim_dat, sim_dat_aux, sim_dat_const, sim_dat_const_aux, 
+                                      timepoints, conv_toll, Ccfl, edges, input_data, 
+                                      rho, total_time, nodes, 
+                                      starts, ends,
+                                      indices_1, indices_2))
+
+config_filename = "test/bifurcation3/bifurcation3.yml"
+(N, B, J, 
+ sim_dat, sim_dat_aux, sim_dat_const, sim_dat_const_aux, 
+ timepoints, conv_toll, Ccfl, edges, input_data, 
+            rho, total_time, nodes, 
+            starts, ends,
+            indices_1, indices_2,
+            vessel_names, cardiac_T) = configSimulation(config_filename)
+sim_loop_old_jit = partial(jit, static_argnums=(0, 1, 2))(simulationLoop)
+sim_dat3, t3, P3  = block_until_ready(sim_loop_old_jit(N, B, J, 
+                                      sim_dat, sim_dat_aux, sim_dat_const, sim_dat_const_aux, 
+                                      timepoints, conv_toll, Ccfl, edges, input_data, 
+                                      rho, total_time, nodes, 
+                                      starts, ends,
+                                      indices_1, indices_2))
+
+r_folder = "results/compare_output_params_results"
+# delete existing folder and results
+if os.path.isdir(r_folder):
+    shutil.rmtree(r_folder)
+os.makedirs(r_folder, mode = 0o777)
+
+filename = config_filename.split("/")[-1]
+network_name = filename.split(".")[0]
+
+#plt.rcParams.update({'font.size': 20})
+
+for i,vessel_name in enumerate(vessel_names):
+    index_vessel_name = vessel_names.index(vessel_name)
+    node = 2
+    index_jax  = 5*index_vessel_name + node
+    _, ax = plt.subplots()
+    ax.set_xlabel("t[s]")
+    ax.set_ylabel("P[mmHg]")
+    plt.plot(t1%cardiac_T,P1[:,index_jax]/133.322)
+    plt.plot(t2%cardiac_T,P2[:,index_jax]/133.322)
+    plt.plot(t3%cardiac_T,P3[:,index_jax]/133.322)
+    plt.legend(["P_1", "P_2", "P_3"], loc="lower right")
+    plt.tight_layout()
+    plt.savefig(r_folder + "/compare_output_params_" + vessel_name + "_P.eps")
+    plt.close()
