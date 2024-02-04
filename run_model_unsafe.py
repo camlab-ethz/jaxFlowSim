293a47109c040fdb7ef1569f998726309306aba1
DiegoRenner <diego.renner@hotmail.com>
2024-02-04 23:53:33 +0100

    updated run_model_unsafe.py for new function signatures

--- run_model_unsafe.py
+++ run_model_unsafe.py
@@ -35,13 +35,14 @@ verbose = True
 (N, B, J, 
  sim_dat, sim_dat_aux, sim_dat_const, sim_dat_const_aux, 
  timepoints, conv_toll, Ccfl, edges, input_data, 
-            rho, total_time, nodes, 
+            rho, nodes, 
             starts, ends,
             indices_1, indices_2,
             vessel_names, cardiac_T) = configSimulation(config_filename, verbose)
 
 if verbose:
     starting_time = time.time_ns()
+
 sim_loop_old_jit = partial(jit, static_argnums=(0, 1,15))(simulationLoopUnsafe)
 sim_dat, P_t, t_t = block_until_ready(sim_loop_old_jit(N, B,
                                       sim_dat, sim_dat_aux, sim_dat_const, sim_dat_const_aux, 
