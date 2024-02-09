79844ae89a5865aa8f9b4cb866b6d2fe652af3b8
DiegoRenner <diego.renner@hotmail.com>
2024-02-09 10:30:57 +0100

    added working conjunction wrappers

--- src/bifurcations.py
+++ src/bifurcations.py
@@ -4,18 +4,18 @@ from jax import lax, jit
 from src.utils import pressure, waveSpeed
 from src.newton import newtonRaphson
 
-@partial(jit, static_argnums=(5))
 def solveBifurcationWrapper(dt, sim_dat, sim_dat_aux, 
                        sim_dat_const, sim_dat_const_aux, 
-                       B, i, index1, edges, starts, ends, rho):
+                       edges, starts, rho, B, ends, i, index2, index3):
+    index1 = ends[i]
     d1_i = edges[i,4]
     d2_i = edges[i,5]
     d1_i_start = starts[d1_i] 
     d2_i_start = starts[d2_i] 
-    u1 = sim_dat[0,index1-1]
+    u1 = sim_dat[0,index1]
     u2 = sim_dat[0,d1_i_start]
     u3 = sim_dat[0,d2_i_start]
-    A1 = sim_dat[2,index1-1]
+    A1 = sim_dat[2,index1]
     A2 = sim_dat[2,d1_i_start]
     A3 = sim_dat[2,d2_i_start]
     (u1, u2, u3, 
@@ -24,16 +24,16 @@ def solveBifurcationWrapper(dt, sim_dat, sim_dat_aux,
      c1, c2, c3, 
      P1, P2, P3) = solveBifurcation(u1, u2, u3, 
                                     A1, A2, A3,
-                                    sim_dat_const[0,index1-1],
+                                    sim_dat_const[0,index1],
                                     sim_dat_const[0,d1_i_start],
                                     sim_dat_const[0,d2_i_start],
-                                    sim_dat_const[1,index1-1],
+                                    sim_dat_const[1,index1],
                                     sim_dat_const[1,d1_i_start],
                                     sim_dat_const[1,d2_i_start],
-                                    sim_dat_const[2,index1-1],
+                                    sim_dat_const[2,index1],
                                     sim_dat_const[2,d1_i_start],
                                     sim_dat_const[2,d2_i_start],
-                                    sim_dat_const[4, index1-1],
+                                    sim_dat_const[4, index1],
                                     sim_dat_const[4, d1_i_start],
                                     sim_dat_const[4, d2_i_start],
                                     )
@@ -42,16 +42,16 @@ def solveBifurcationWrapper(dt, sim_dat, sim_dat_aux,
     temp3 = jnp.array((u3, Q3, A3, c3, P3))
     sim_dat = lax.dynamic_update_slice( 
         sim_dat, 
-        temp1[:,jnp.newaxis]*jnp.ones(B+1)[jnp.newaxis,:],
-        (0,index1-1))
+        temp1[:,jnp.newaxis]*jnp.ones(3)[jnp.newaxis,:],
+        (0,index1))
     sim_dat = lax.dynamic_update_slice( 
         sim_dat, 
-        temp2[:,jnp.newaxis]*jnp.ones(B+1)[jnp.newaxis,:],
-        (0,d1_i_start-B))
+        temp2[:,jnp.newaxis]*jnp.ones(3)[jnp.newaxis,:],
+        (0,d1_i_start-2))
     sim_dat = lax.dynamic_update_slice( 
         sim_dat, 
-        temp3[:,jnp.newaxis]*jnp.ones(B+1)[jnp.newaxis,:],
-        (0,d2_i_start-B))
+        temp3[:,jnp.newaxis]*jnp.ones(3)[jnp.newaxis,:],
+        (0,d2_i_start-2))
     return sim_dat, sim_dat_aux
 
 def solveBifurcation(u1, u2, u3, 
@@ -74,9 +74,9 @@ def solveBifurcation(u1, u2, u3,
     J = calculateJacobianBifurcation(U0, k,
                                      A01, A02, A03,
                                      beta1, beta2, beta3)
-    U = newtonRaphson(calculateWstarBifurcation, 
+    U = newtonRaphson(#calculateWstarBifurcation, 
                       calculateFBifurcation, 
-                      J, U0, k,
+                      J, U0, 
                       (A01, A02, A03),
                       (beta1, beta2, beta3))
 
@@ -115,18 +115,18 @@ def calculateJacobianBifurcation(U, k,
                       [0.0, 0.0, 1.0, 0.0, 0.0, J36],
                       [J41, J42, J43, J44, J45, J46],
                       [0.0, 0.0, 0.0, J54, J55, 0.0],
-                      [0.0, 0.0, 0.0, J64, 0.0, J66]], dtype=jnp.float64)
+                      [0.0, 0.0, 0.0, J64, 0.0, J66]])
 
 
-def calculateWstarBifurcation(U, k):
-    W1 = U[0] + 4.0 * k[0] * U[3]
-    W2 = U[1] - 4.0 * k[1] * U[4]
-    W3 = U[2] - 4.0 * k[2] * U[5]
+#def calculateWstarBifurcation(U, k):
+#    W1 = U[0] + 4.0 * k[0] * U[3]
+#    W2 = U[1] - 4.0 * k[1] * U[4]
+#    W3 = U[2] - 4.0 * k[2] * U[5]
+#
+#    return jnp.array([W1, W2, W3])
 
-    return jnp.array([W1, W2, W3], dtype=jnp.float64)
 
-
-def calculateFBifurcation(U, k, W,
+def calculateFBifurcation(U,# k, W,
                           A0s,
                           betas):
 
@@ -140,15 +140,15 @@ def calculateFBifurcation(U, k, W,
     U52 = U[4]*U[4]
     U62 = U[5]*U[5]
 
-    f1 = U[0] + 4.0 * k[0] * U[3] - W[0]
-    f2 = U[1] - 4.0 * k[1] * U[4] - W[1]
-    f3 = U[2] - 4.0 * k[2] * U[5] - W[2]
+    f1 = 0 #U[0] + 4.0 * k[0] * U[3] - W[0]
+    f2 = 0 #U[1] - 4.0 * k[1] * U[4] - W[1]
+    f3 = 0 #U[2] - 4.0 * k[2] * U[5] - W[2]
     f4 = U[0] * (U42*U42) - U[1] * (U52*U52) - U[2] * (U62*U62)
 
     f5 = beta1 * (U42 * jnp.sqrt(1/A01) - 1.0) - (beta2 * (U52 * jnp.sqrt(1/A02) - 1.0))
     f6 = beta1 * (U42 * jnp.sqrt(1/A01) - 1.0) - (beta3 * (U62 * jnp.sqrt(1/A03) - 1.0))
 
-    return jnp.array([f1, f2, f3, f4, f5, f6], dtype=jnp.float64)
+    return jnp.array([f1, f2, f3, f4, f5, f6])
 
 
 def updateBifurcation(U,
