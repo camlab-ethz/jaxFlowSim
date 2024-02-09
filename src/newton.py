eb546a2a9b6026eda9d204f5891c7e71a03dd035
DiegoRenner <diego.renner@hotmail.com>
2024-02-09 10:39:18 +0100

    reverted newton solver back to safe version

--- src/newton.py
+++ src/newton.py
@@ -15,28 +15,27 @@ def newtonRaphson(fun_w, fun_f, J,
             
     dU = jnp.linalg.solve(J, -F)
     n = F.size
-    #ones = jnp.ones(n,dtype=jnp.int32)
-    #zeros = jnp.zeros(n,dtype=jnp.int32)
+    ones = jnp.ones(n,dtype=jnp.int32)
+    zeros = jnp.zeros(n,dtype=jnp.int32)
 
-    #def cond_fun(args):
-    #    _, dU, F, _  = args
-    #    ret = lax.cond(jnp.where((jnp.abs(dU) <= nr_toll_U) | (jnp.abs(F) <= nr_toll_F), 
-    #                                  ones,
-    #                                  zeros).sum()==n,
-    #                        lambda: False,
-    #                        lambda: True)
-    #    return ret
+    def cond_fun(args):
+        _, dU, F, _  = args
+        ret = lax.cond(jnp.where((jnp.abs(dU) <= nr_toll_U) | (jnp.abs(F) <= nr_toll_F), 
+                                      ones,
+                                      zeros).sum()==n,
+                            lambda: False,
+                            lambda: True)
+        return ret
 
-    #def body_fun(args):
-    #    U, _, _, counter = args
-    #    W = fun_w(U, k)
-    #    F = fun_f(U, k, W,
-    #              A0s,
-    #              betas)
-    #    dU = jnp.linalg.solve(J, -F)
-    #    counter += 1
-    #    return U+dU, dU, F, counter
+    def body_fun(args):
+        U, _, _, counter = args
+        W = fun_w(U, k)
+        F = fun_f(U, k, W,
+                  A0s,
+                  betas)
+        dU = jnp.linalg.solve(J, -F)
+        counter += 1
+        return U+dU, dU, F, counter
 
-    #counter = 0
-    #return lax.while_loop(cond_fun, body_fun, (U+dU, dU, F, counter))
-    return U+dU
+    counter = 0
+    return lax.while_loop(cond_fun, body_fun, (U+dU, dU, F, counter))
