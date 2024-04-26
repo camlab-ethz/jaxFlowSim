4052277bcb2ccd95149172c068c7fc6e1f793557
Diego Renner <diego.renner@hotmail.com>
2024-04-26 19:02:42 +0200

    cleaned up old inference scripts

--- run_inference.sh
+++ run_inference.sh
@@ -5,7 +5,7 @@ source .venv/bin/activate
 for i in {0..15}
 do
 	#screen -dmS inference_ensemble$i "source venv/bin/activate; python run_inference6.py bifurcation $i" &
-	screen -dmS inference_ensemble$i bash -c "source .venv/bin/activate; python run_inference6.py bifurcation $i" & 
+	screen -dmS inference_ensemble$i bash -c "source .venv/bin/activate; python run_inference.py bifurcation $i" & 
 	sleep 10;
 	#python run_inference6.py bifurcation $i &
 done
