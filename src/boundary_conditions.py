d246d4dc0b955bf65a1521ea681ef5f58ede67df
DiegoRenner <diego.renner@hotmail.com>
2024-05-16 18:27:31 +0200

    added debugging to boundary conditions

--- src/boundary_conditions.py
+++ src/boundary_conditions.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
-from jax import lax
+from jax import lax, debug
 from src.utils import pressure
 
 def setInletBC(inlet, u0, u1, 
@@ -154,6 +154,10 @@ def threeElementWindkessel(dt, u1, A1,
                            Pc, Cc, R1, 
                            R2, beta, gamma, 
                            A0, Pext):
+    debug.print("{x}", x = (dt, u1, A1, 
+                           Pc, Cc, R1, 
+                           R2, beta, gamma, 
+                           A0, Pext))
     Pout = 0.0
 
     Al = A1
