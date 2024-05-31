6a1838ba67067b0b5d480ea9529bd70e760b9d9c
Diego Renner <diego.renner@hotmail.com>
2024-05-31 21:44:00 +0200

    added ensemble training for different optax gradient transformations

--- run_inference_sgd.py
+++ run_inference_sgd.py
@@ -10,6 +10,8 @@ import jax.numpy as jnp
 import jax
 import numpy as np
 import itertools
+import optax
+from flax.training.train_state import TrainState
 
 os.chdir(os.path.dirname(__file__))
 jax.config.update("jax_enable_x64", True)
@@ -63,7 +65,8 @@ var_index = 7
 R1 = sim_dat_const[var_index,strides[R_index,1]]
 #R_scales = np.linspace(1.1*R1, 2*R1, 16)
 R_scales = np.linspace(0.1*R1, 10*R1, 16)
-def simLoopWrapper(R):
+def simLoopWrapper(params):
+    R = params[0]
     ones = jnp.ones(strides[R_index,1]-strides[R_index,0]+4)
     sim_dat_const_new = jnp.array(sim_dat_const)
     sim_dat_const_new = sim_dat_const_new.at[var_index,strides[R_index,0]-2:strides[R_index,1]+2].set(R*ones)
@@ -76,27 +79,66 @@ def simLoopWrapper(R):
     #jax.debug.print("P={x}", x=P)
     #jax.debug.print("R={x}", x=R)
     #jax.debug.print("P_obs={x}", x=P_obs)
-    return jnp.sqrt(jnp.sum(jnp.square((P-P_obs))))
+    return P
 
-print(simLoopWrapper(R1))
-sim_loop_wrapper_jit = jit(jacfwd(simLoopWrapper))
-print(sim_loop_wrapper_jit(R1))
 results_folder = "results/inference_ensemble_det"
 if not os.path.isdir(results_folder):
     os.makedirs(results_folder, mode = 0o777)
 
 learning_rates = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e2, 1e3, 1e4, 1e5]
-for (j, learning_rate) in enumerate(learning_rates):
-    R_star = R_scales[int(sys.argv[2])]
-    for i in range(1000):
-        print(j,i)
-        gradient = sim_loop_wrapper_jit(R_star)
-        R_star -= learning_rate*gradient
 
-    results_file = results_folder  + "/setup_" + str(learning_rate) + ".txt"
+network_properties = {
+        "tx": [optax.adam, optax.sgd, optax.lars],
+        "learning_rate": [1e-5, 1e-3, 1e-1, 1e2, 1e4],
+        "epochs": [100,1000,2000]
+        }
+
+settings = list(itertools.product(*network_properties.values()))
+
+results_folder = "results/inference_ensemble_sgd"
+if not os.path.isdir(results_folder):
+    os.makedirs(results_folder, mode = 0o777)
+
+for set_num, setup in enumerate(settings):
+    print("###################################", set_num, "###################################")
+    results_file = results_folder  + "/setup_" + str(setup[0].__name__) + "_" + str(setup[1]) +  "_" + str(setup[2]) +".txt"
+
+    model = simLoopWrapper
+    variables = [R_scales[int(sys.argv[2])]]
+    tx = setup[0]
+    y = P_obs
+    x = simLoopWrapper
+
+    state = TrainState.create(
+        apply_fn=model,
+        params=variables,
+        tx=tx(setup[1]))
+
+    def loss_fn(params, x, y):
+      predictions = state.apply_fn(params)
+      loss = optax.l2_loss(predictions=predictions, targets=y).mean()
+      return loss
+
+
+    for _ in range(setup[2]):
+        grads = jax.jacfwd(loss_fn)(state.params, x, y)
+        state = state.apply_gradients(grads=grads)
+        print(loss_fn(state.params, x, y))
     file = open(results_file, "a")  
-    file.write(str(R_scales[int(sys.argv[2])]) + " " + str(R_star) + "  " + str(R1) + "\n")
+    file.write(str(R_scales[int(sys.argv[2])]) + " " + str(state.params[0]) + "  " + str(R1) + "\n")
     file.close()
+
+#for (j, learning_rate) in enumerate(learning_rates):
+#    R_star = R_scales[int(sys.argv[2])]
+#    for i in range(1000):
+#        print(j,i)
+#        gradient = sim_loop_wrapper_jit(R_star)
+#        R_star -= learning_rate*gradient
+#
+#    results_file = results_folder  + "/setup_" + str(learning_rate) + ".txt"
+#    file = open(results_file, "a")  
+#    file.write(str(R_scales[int(sys.argv[2])]) + " " + str(R_star) + "  " + str(R1) + "\n")
+#    file.close()
 
     
 
@@ -108,42 +150,3 @@ network_properties = {
         "num_samples": np.arange(100, 1100, 100),
         "num_chains": [1]
         }
-
-#network_properties = {
-#        "sigma": [1e-5],
-#        "scale": [10],
-#        "num_warmup": np.arange(10, 20, 10),
-#        "num_samples": np.arange(10, 20, 10),
-#        "num_chains": [1]
-#        }
-
-#settings = list(itertools.product(*network_properties.values()))
-#
-#results_folder = "results/inference_ensemble"
-#if not os.path.isdir(results_folder):
-#    os.makedirs(results_folder, mode = 0o777)
-#
-#for set_num, setup in enumerate(settings):
-#    print("###################################", set_num, "###################################")
-#    setup_properties = {
-#            "sigma": setup[0],
-#            "scale": setup[1],
-#            "num_warmup": setup[2],
-#            "num_samples": setup[3],
-#            "num_chains": setup[4]
-#            }
-#    results_file = results_folder  + "/setup_" + str(setup_properties["sigma"]) + "_" + str(setup_properties["scale"]) + "_" + str(setup_properties["num_warmup"]) + "_" + str(setup_properties["num_samples"]) + "_" + str(setup_properties["num_chains"]) + ".txt"
-#    mcmc = MCMC(numpyro.infer.NUTS(model, 
-#                                   forward_mode_differentiation=True),
-#                                   num_samples=setup_properties["num_samples"],
-#                                   num_warmup=setup_properties["num_warmup"],
-#                                   num_chains=setup_properties["num_chains"])
-#    mcmc.run(jax.random.PRNGKey(3450), 
-#             P_obs, setup_properties["sigma"], setup_properties["scale"], R_scale)
-#    mcmc.print_summary()
-#    R = jnp.mean(mcmc.get_samples()["theta"])
-#
-#    #print(mcmc.get_samples())
-#    file = open(results_file, "a")  
-#    file.write(str(R_scale) + " " + str(R) + "  " + str(R1) + "\n")
-#    file.close()
