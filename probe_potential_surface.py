fixing gradients

probe_potential_surface.py
@@ -64,7 +64,12 @@ var_index = 7
 R1 = sim_dat_const[var_index,strides[R_index,1]]
 #R_scales = np.linspace(1.1*R1, 2*R1, 16)
 R_scales = np.linspace(0.1, 10, int(1e6))
-def simLoopWrapper(R):
+def simLoopWrapper(R, R_index, R1, var_index, P_obs, N, B,
+                                          sim_dat, sim_dat_aux, 
+                                          sim_dat_const, sim_dat_const_aux, 
+                                          Ccfl, input_data, rho, 
+                                          masks, strides, edges,
+                                          upper=120000):
     R=R*R1
     ones = jnp.ones(strides[R_index,1]-strides[R_index,0]+4)
     sim_dat_const_new = jnp.array(sim_dat_const)
@@ -77,8 +82,8 @@ def simLoopWrapper(R):
                                           upper=120000)
     return jnp.sqrt(jnp.sum(jnp.square((P-P_obs))))/jnp.sqrt(jnp.sum(jnp.square((P_obs))))
 
-sim_loop_wrapper_jit = jit(simLoopWrapper)
-sim_loop_wrapper_grad_jit = jit(jacfwd(simLoopWrapper))
+sim_loop_wrapper_jit = partial(jit, static_argnums=(1, 5, 6, 17))(simLoopWrapper)
+sim_loop_wrapper_grad_jit = jit(jacfwd(simLoopWrapper, 0))
 
 results_folder = "results/potential_surface"
 if not os.path.isdir(results_folder):
@@ -87,10 +92,20 @@ if not os.path.isdir(results_folder):
 slices = int(1e6/int(sys.argv[3]))
 
 for i in range(int(sys.argv[2])*slices,(int(sys.argv[2])+1)*slices):
-    results_file = results_folder  + "/potential_surface.txt"
+    results_file = results_folder  + "/potential_surface_new.txt"
     R = R_scales[i]
-    loss = sim_loop_wrapper_jit(R)
-    gradient = sim_loop_wrapper_grad_jit(R)
+    loss = sim_loop_wrapper_jit(R, R_index, R1, var_index, P_obs, N, B,
+                                          sim_dat, sim_dat_aux, 
+                                          sim_dat_const, sim_dat_const_aux, 
+                                          Ccfl, input_data, rho, 
+                                          masks, strides, edges,
+                                          upper=120000)
+    gradient = sim_loop_wrapper_grad_jit(R, R_index, R1, var_index, P_obs, N, B,
+                                          sim_dat, sim_dat_aux, 
+                                          sim_dat_const, sim_dat_const_aux, 
+                                          Ccfl, input_data, rho, 
+                                          masks, strides, edges,
+                                          upper=120000)
     file = open(results_file, "a")  
     file.write(str(R) + " " + str(loss) + " " + str(gradient) + "\n")
     file.close()