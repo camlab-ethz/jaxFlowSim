faa09f9ceb92c639cdd1742ccb29143a20daed05
Diego Renner <diego.renner@hotmail.com>
2024-07-21 21:01:10 +0200

    modified ccfl for probe potential surface

--- probe_potential_surface.py
+++ probe_potential_surface.py
@@ -58,7 +58,7 @@ sim_loop_old_jit = partial(jit, static_argnums=(0, 1, 12))(simulationLoopUnsafe)
 sim_dat_obs, t_obs, P_obs = sim_loop_old_jit(N, B,
                                       sim_dat, sim_dat_aux, 
                                       sim_dat_const, sim_dat_const_aux, 
-                                      Ccfl, input_data, rho, 
+                                      0.5, input_data, rho, 
                                       masks, strides, edges,
                                       num_iterations)
 
@@ -85,9 +85,9 @@ def simLoopWrapper(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
     _, _, P = sim_loop_old_jit(N, B,
                                           sim_dat, sim_dat_aux, 
                                           sim_dat_const_new, sim_dat_const_aux, 
-                                          Ccfl, input_data, rho, 
+                                          0.5, input_data, rho, 
                                           masks, strides, edges,
-                                          200)
+                                          120000)
     return jnp.sqrt(jnp.sum(jnp.square((P-P_obs))))/jnp.sqrt(jnp.sum(jnp.square((P_obs))))
 def simLoopWrapper1(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
                                           sim_dat, sim_dat_aux, 
@@ -106,7 +106,7 @@ def simLoopWrapper1(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
                                           sim_dat_const_new, sim_dat_const_aux, 
                                           Ccfl, input_data, rho, 
                                           masks, strides, edges,
-                                          200)
+                                          120000)
     return sim_dat
 
 sim_loop_wrapper_jit = partial(jit, static_argnums=(1, 5, 6, 7, 8, 9))(simLoopWrapper)
@@ -122,71 +122,71 @@ slices = int(total_num_points/int(sys.argv[3]))
 gradients = np.zeros(slices)
 gradients_averaged = np.zeros(slices)
 gradient = 1
-for i in range(1):
+#for i in range(1):
+#    results_file = results_folder  + "/potential_surface_new.txt"
+#    R = R_scales[100]
+#    M = strides[R_index,1]-strides[R_index,0]+4
+#    start = strides[R_index,0]-2
+#    end = strides[R_index,1]+2
+#    sim_dat = simLoopWrapper1(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
+#                                          sim_dat, sim_dat_aux, 
+#                                          sim_dat_const, sim_dat_const_aux, 
+#                                          Ccfl, input_data, rho, 
+#                                          masks, strides, edges)
+#    gradient *= sim_loop_wrapper_grad_jit1(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
+#                                          sim_dat, sim_dat_aux, 
+#                                          sim_dat_const, sim_dat_const_aux, 
+#                                          Ccfl, input_data, rho, 
+#                                          masks, strides, edges)
+#    #gradient *= sim_loop_wrapper_grad_jit(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
+#    #                                      sim_dat, sim_dat_aux, 
+#    #                                      sim_dat_const, sim_dat_const_aux, 
+#    #                                      Ccfl, input_data, rho, 
+#    #                                      masks, strides, edges)
+#    print(gradient)
+
+
+for i in range(int(sys.argv[2])*slices,(int(sys.argv[2])+1)*slices):
     results_file = results_folder  + "/potential_surface_new.txt"
-    R = R_scales[100]
+    R = R_scales[i]
     M = strides[R_index,1]-strides[R_index,0]+4
     start = strides[R_index,0]-2
     end = strides[R_index,1]+2
-    sim_dat = simLoopWrapper1(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
+    loss = sim_loop_wrapper_jit(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
                                           sim_dat, sim_dat_aux, 
                                           sim_dat_const, sim_dat_const_aux, 
                                           Ccfl, input_data, rho, 
                                           masks, strides, edges)
-    gradient *= sim_loop_wrapper_grad_jit1(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
+    sim_loop_wrapper_R = Partial(sim_loop_wrapper_jit, R_index=R_index, R1=R1, var_index=var_index, P_obs=P_obs, N=N, B=B, M=M, start=start, end=end,
+                                          sim_dat=sim_dat, sim_dat_aux=sim_dat_aux, 
+                                          sim_dat_const=sim_dat_const, sim_dat_const_aux=sim_dat_const_aux, 
+                                          Ccfl=Ccfl, input_data=input_data, rho=rho, 
+                                          masks=masks, strides=strides, edges=edges)
+
+    M = strides[R_index,1]-strides[R_index,0]+4
+    start = strides[R_index,0]-2
+    end = strides[R_index,1]+2
+    gradients[i-int(sys.argv[2])*slices]= sim_loop_wrapper_grad_jit(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
                                           sim_dat, sim_dat_aux, 
                                           sim_dat_const, sim_dat_const_aux, 
                                           Ccfl, input_data, rho, 
                                           masks, strides, edges)
-    #gradient *= sim_loop_wrapper_grad_jit(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
-    #                                      sim_dat, sim_dat_aux, 
-    #                                      sim_dat_const, sim_dat_const_aux, 
-    #                                      Ccfl, input_data, rho, 
-    #                                      masks, strides, edges)
-    print(gradient)
+    print(loss, gradients[i-int(sys.argv[2])*slices])
+    check_grads(sim_loop_wrapper_R, (R,), order=1, atol=1e-2, rtol=1e-2, modes='fwd')
+
+    
+    #if abs(gradients[i-int(sys.argv[2])*slices]) > 1e3:
+    #    gradients[i-int(sys.argv[2])*slices] = np.sign(gradients[i-int(sys.argv[2])*slices])*1e3
 
 
-#for i in range(int(sys.argv[2])*slices,(int(sys.argv[2])+1)*slices):
-#    results_file = results_folder  + "/potential_surface_new.txt"
-#    R = R_scales[i]
-#    M = strides[R_index,1]-strides[R_index,0]+4
-#    start = strides[R_index,0]-2
-#    end = strides[R_index,1]+2
-#    loss = sim_loop_wrapper_jit(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
-#                                          sim_dat, sim_dat_aux, 
-#                                          sim_dat_const, sim_dat_const_aux, 
-#                                          Ccfl, input_data, rho, 
-#                                          masks, strides, edges)
-#    print(loss)
-#    sim_loop_wrapper_R = Partial(sim_loop_wrapper_jit, R_index=R_index, R1=R1, var_index=var_index, P_obs=P_obs, N=N, B=B, M=M, start=start, end=end,
-#                                          sim_dat=sim_dat, sim_dat_aux=sim_dat_aux, 
-#                                          sim_dat_const=sim_dat_const, sim_dat_const_aux=sim_dat_const_aux, 
-#                                          Ccfl=Ccfl, input_data=input_data, rho=rho, 
-#                                          masks=masks, strides=strides, edges=edges)
-#    check_grads(sim_loop_wrapper_R, (R,), order=1)
-#
-#    M = strides[R_index,1]-strides[R_index,0]+4
-#    start = strides[R_index,0]-2
-#    end = strides[R_index,1]+2
-#    gradients[i-int(sys.argv[2])*slices]= sim_loop_wrapper_grad_jit(R, R_index, R1, var_index, P_obs, N, B, M, start, end,
-#                                          sim_dat, sim_dat_aux, 
-#                                          sim_dat_const, sim_dat_const_aux, 
-#                                          Ccfl, input_data, rho, 
-#                                          masks, strides, edges)
-#
-#    
-#    #if abs(gradients[i-int(sys.argv[2])*slices]) > 1e3:
-#    #    gradients[i-int(sys.argv[2])*slices] = np.sign(gradients[i-int(sys.argv[2])*slices])*1e3
-#
-#
-#    if i >= int(sys.argv[2])*slices + 1000:
-#        gradients_averaged[i-int(sys.argv[2])*slices] = gradients[i-int(sys.argv[2])*slices-999:i-int(sys.argv[2])*slices+1].mean()
-#        gradients_averaged[i-int(sys.argv[2])*slices] = gradients[i-int(sys.argv[2])*slices-999:i-int(sys.argv[2])*slices+1].mean()
-#    else:
-#        gradients_averaged[i-int(sys.argv[2])*slices] = gradients[:i-int(sys.argv[2])*slices+1].mean()
-#
-#
-#       
-#    file = open(results_file, "a")  
-#    file.write(str(R) + " " + str(loss) + " " + str(gradients[i-int(sys.argv[2])*slices]) + " " + str(gradients_averaged[i-int(sys.argv[2])*slices]) + "\n")
-#    file.close()
+    if i >= int(sys.argv[2])*slices + 1000:
+        gradients_averaged[i-int(sys.argv[2])*slices] = gradients[i-int(sys.argv[2])*slices-999:i-int(sys.argv[2])*slices+1].mean()
+        gradients_averaged[i-int(sys.argv[2])*slices] = gradients[i-int(sys.argv[2])*slices-999:i-int(sys.argv[2])*slices+1].mean()
+    else:
+        gradients_averaged[i-int(sys.argv[2])*slices] = gradients[:i-int(sys.argv[2])*slices+1].mean()
+
+
+       
+    file = open(results_file, "a")  
+    file.write(str(R) + " " + str(loss) + " " + str(gradients[i-int(sys.argv[2])*slices]) + " " + str(gradients_averaged[i-int(sys.argv[2])*slices]) + "\n")
+    file.close()
