617e27ad89b57f23f76046d180383000d361083f
DiegoRenner <diego.renner@hotmail.com>
2024-08-02 18:27:03 +0200

    cleaned up components

--- src/components.py
+++ src/components.py
@@ -1,8 +1,38 @@
+"""
+This module provides a class to represent the properties of blood in a vascular network model using JAX.
+
+It includes a class to:
+- Define the properties of blood (`Blood`).
+
+The module makes use of the following imported utilities:
+- `jaxtyping` and `typeguard` for type checking and ensuring type safety in the functions.
+"""
+
+from jaxtyping import Array, Float, jaxtyped
+from typeguard import typechecked as typechecker
+
+
+@jaxtyped(typechecker=typechecker)
 class Blood:
-    mu: float
-    rho: float
-    rho_inv: float
-    def __init__(self, mu, rho, rho_inv):
+    """
+    A class to represent the properties of blood.
+
+    Attributes:
+    mu (Float[Array, ""]): Dynamic viscosity of blood.
+    rho (Float[Array, ""]): Density of blood.
+    rho_inv (Float[Array, ""]): Inverse density of blood.
+    """
+
+    mu: Float[Array, ""]
+    rho: Float[Array, ""]
+    rho_inv: Float[Array, ""]
+
+    def __init__(
+        self,
+        mu: Float[Array, ""],
+        rho: Float[Array, ""],
+        rho_inv: Float[Array, ""],
+    ):
         self.mu = mu
         self.rho = rho
         self.rho_inv = rho_inv
