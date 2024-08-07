aab312a7bd3be064cd815bb74dee81d3843215b4
DiegoRenner <diego.renner@hotmail.com>
2024-08-08 00:20:35 +0200

    cleaned up run_inference_numpyro.sh

--- run_inference_numpyro.sh
+++ run_inference_numpyro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This script starts multiple detached screen sessions to run the 'run_inference_optax.py' script
-# for an ensemble training of inferences based on optimizers provided by optax.
+# This script starts multiple detached screen sessions to run the 'run_inference_numpyro.py' script
+# for an ensemble training of inferences based on the NUTS optimizer provided by numpyro.
 # Each process is started with a different initial value according to the num_process variable.
 
 # Activate the virtual environment
@@ -16,12 +16,13 @@ do
     echo "Starting process $i"
 
 	# Start a new detached screen session for each process
-    # The session is named 'inference_ensemble_optax' followed by the process number
-    # Run the Python script 'run_inference_optax.py' with arguments 'bifurcation' and the process number
-	screen -dmS inference_ensemble_optax"$i" bash -c "source venv/bin/activate; python run_inference_optax.py bifurcation $i" & 
-	
+    # The session is named 'inference_ensemble_numpyro' followed by the process number
+    # Run the Python script 'run_inference_numpyro.py' with arguments 'bifurcation' and the process number
+	screen -dmS inference_ensemble_numpyro"$i" bash -c "source .venv/bin/activate; python run_inference_nupyro.py bifurcation $i" & 
+
 	# Sleep for 10 seconds to prevent race conditions or resource contention
 	sleep 10;
 done
 
 echo "All processes started."
+
