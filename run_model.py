b834c04bc041034c63bc26fb2fa8fc642eae345d
DiegoRenner <diego.renner@hotmail.com>
2024-08-09 00:03:25 +0200

    formatting and refactoring of run_model.py

--- run_model.py
+++ run_model.py
@@ -1,39 +1,69 @@
-from src.model import config_simulation, simulation_loop
-import jax
+"""
+This script performs vascular network simulations using JAX, compares the simulation results with reference data,
+and generates plots showing the relative error between the simulation and reference pressures for each vessel in the network.
+
+The script includes the following steps:
+1. Set up the environment and configuration.
+2. Load the vascular network model based on the provided configuration file.
+3. Run the simulation loop using JAX.
+4. Compare the simulation results with reference data and calculate the relative error.
+5. Plot the pressure data from the simulation and the reference data, and save the plots as PDF files.
+
+Modules:
+    - os: Provides functions for interacting with the operating system.
+    - sys: Provides access to some variables used or maintained by the interpreter.
+    - time: Contains functions for time-related operations.
+    - functools.partial: Allows partial function application.
+    - jax: The JAX library for high-performance numerical computing.
+    - matplotlib.pyplot: A plotting library for visualizing data.
+    - numpy: A library for numerical computations.
+    - src.model: Custom module containing functions for simulation configuration and execution.
+
+Functions:
+    - config_simulation: Configures the simulation parameters based on the model's configuration file.
+    - simulation_loop: Runs the main simulation loop.
+
+Global Variables:
+    - CONFIG_FILENAME (str): The configuration file path for the simulation.
+    - VERBOSE (bool): Controls whether to print detailed logs and timing information.
+
+Usage:
+    To execute the script, run it with an optional model name argument:
+        python script.py [model_name]
+    If no model name is provided, the script defaults to using "test/adan56/adan56.yml".
+    The script will generate and save plots of the simulation results in the 'results' directory.
+"""
+
+import os
 import sys
 import time
-import os
 from functools import partial
-from jax import block_until_ready, jit
+
+import jax
 import matplotlib.pyplot as plt
 import numpy as np
+from jax import block_until_ready, jit
 
+from src.model import config_simulation, simulation_loop
+
+# Change directory to the script's location
 os.chdir(os.path.dirname(__file__))
+
+# Enable 64-bit precision in JAX
 jax.config.update("jax_enable_x64", True)
 
-config_filename = ""
+# Determine the configuration filename based on command line arguments
+CONFIG_FILENAME = ""
 if len(sys.argv) == 1:
-
-    # base cases
-    # modelname = "single-artery"
-    # modelname = "tapering"
-    # modelname = "conjunction"
-    # modelname = "bifurcation"
-    # modelname = "aspirator"
-
-    # openBF-hub
-    modelname = "test/adan56/adan56.yml"
-
-    # vascularmodels.com
-    # modelname = "0007_H_AO_H"
-    # modelname = "0029_H_ABAO_H"
-    # modelname = "0053_H_CERE_H"
-    input_filename = "test/" + modelname + "/" + modelname + ".yml"
-
+    MODELNAME = "test/adan56/adan56.yml"
+    CONFIG_FILENAME = "test/" + MODELNAME + "/" + MODELNAME + ".yml"
 else:
-    config_filename = "test/" + sys.argv[1] + "/" + sys.argv[1] + ".yml"
+    CONFIG_FILENAME = "test/" + sys.argv[1] + "/" + sys.argv[1] + ".yml"
 
-verbose = True
+# Set verbosity flag to control logging
+VERBOSE = True
+
+# Configure the simulation with the given configuration file
 (
     N,
     B,
@@ -52,13 +82,19 @@ verbose = True
     edges,
     vessel_names,
     cardiac_T,
-) = config_simulation(config_filename, verbose)
+) = config_simulation(CONFIG_FILENAME, VERBOSE)
 
-if verbose:
-    starting_time = time.time_ns()
-sim_loop_old_jit = partial(jit, static_argnums=(0, 1, 2))(simulation_loop)
+# Initialize timing variables
+STARTING_TIME = 0.0
+
+# If verbose mode is enabled, record the start time
+if VERBOSE:
+    STARTING_TIME = time.time_ns()
+
+# Set up and execute the simulation loop using JIT compilation
+SIM_LOOP_JIT = partial(jit, static_argnums=(0, 1, 2))(simulation_loop)
 sim_dat, t, P = block_until_ready(
-    sim_loop_old_jit(
+    SIM_LOOP_JIT(  # pylint: disable=E1102
         N,
         B,
         J,
@@ -77,19 +113,16 @@ sim_dat, t, P = block_until_ready(
     )
 )
 
-if verbose:
-    ending_time = (time.time_ns() - starting_time) / 1.0e9
+# If verbose mode is enabled, calculate and print the elapsed time
+if VERBOSE:
+    ending_time = (time.time_ns() - STARTING_TIME) / 1.0e9
     print(f"elapsed time = {ending_time} seconds")
 
-# save data for unittests
-# np.savetxt("test/test_data/bifurcation_sim_dat.dat", sim_dat)
-# np.savetxt("test/test_data/bifurcation_t.dat", t)
-# np.savetxt("test/test_data/bifurcation_P.dat", P)
-
-# jnp.set_printoptions(threshold=sys.maxsize)
-filename = config_filename.split("/")[-1]
+# Extract the network name from the configuration filename
+filename = CONFIG_FILENAME.split("/")[-1]
 network_name = filename.split(".")[0]
 
+# Define vessel names for different configurations (example data)
 vessel_names_0007 = [
     "ascending aorta",
     "right subclavian artery",
@@ -142,9 +175,7 @@ vessel_names_0053 = [
     "right posterior comunicating artery",
 ]
 
-# plt.rcParams.update({'font.size': 20})
-
-
+# Loop through each vessel name and compare the simulation results with reference data
 for i, vessel_name in enumerate(vessel_names):
     index_vessel_name = vessel_names.index(vessel_name)
     P0 = np.loadtxt(
@@ -156,10 +187,10 @@ for i, vessel_name in enumerate(vessel_names):
         + vessel_name
         + "_P.last"
     )
-    node = 2
-    index_jl = 1 + node
-    index_jax = 5 * index_vessel_name + node
-    P0 = P0[:, index_jl]
+    NODE = 2
+    INDEX_JL = 1 + NODE
+    index_jax = 5 * index_vessel_name + NODE
+    P0 = P0[:, INDEX_JL]
     res = np.sqrt(((P[:, index_jax] - P0).dot(P[:, index_jax] - P0) / P0.dot(P0)))
     _, ax = plt.subplots()
     ax.set_xlabel("t[s]")
@@ -170,18 +201,13 @@ for i, vessel_name in enumerate(vessel_names):
         + ", # vessels: "
         + str(N)
         + ", vessel name: "
-        + vessel_names[i]
+        + vessel_name
         + ", \n relative error = |P_JAX-P_jl|/|P_jl| = "
         + str(res)
         + "%"
     )
-    # plt.title("network: " + network_name + ", vessel name: " + vessel_names_0053[i])
-    # plt.title(vessel_names_0053[i])
-    # plt.title("vessel name: " + vessel_name)
     plt.plot(t % cardiac_T, P[:, index_jax] / 133.322)
     plt.plot(t % cardiac_T, P0 / 133.322)
-    # plt.legend(["P_JAX", "P_jl"], loc="lower right")
-    # plt.axis("off")
     plt.tight_layout()
     plt.savefig(
         "results/"
@@ -191,5 +217,5 @@ for i, vessel_name in enumerate(vessel_names):
         + "_"
         + vessel_names[i].replace(" ", "_")
         + "_P.pdf"
-    )  # , bbox_inches='tight')
+    )
     plt.close()
