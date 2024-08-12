5c08ceced5bf82360d828f82f8d24a92c6a3fb04
DiegoRenner <diego.renner@hotmail.com>
2024-08-12 22:34:22 +0200

    refactored steps_opt

--- demos/steps_opt.py
+++ demos/steps_opt.py
@@ -42,10 +42,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from jax import block_until_ready, jit
 
+sys.path.insert(0, sys.path[0] + "/..")
 from src.model import config_simulation, simulation_loop, simulation_loop_unsafe
 
 # Change the current working directory to the script's location
-os.chdir(os.path.dirname(__file__))
+os.chdir(os.path.dirname(__file__) + "/..")
 
 # Enable 64-bit precision in JAX
 jax.config.update("jax_enable_x64", True)
@@ -54,7 +55,7 @@ jax.config.update("jax_enable_x64", True)
 CONFIG_FILENAME = ""
 if len(sys.argv) == 1:
 
-    MODELNAME = "test/adan56/adan56.yml"
+    MODELNAME = "adan56"
 
     CONFIG_FILENAME = "test/" + MODELNAME + "/" + MODELNAME + ".yml"
 else:
@@ -164,7 +165,7 @@ sim_dat_out, t_out, P_out = block_until_ready(
         sim_dat_const,
         sim_dat_const_aux,
         timepoints,
-        conv_tol,
+        float(conv_tol),
         Ccfl,
         input_data,
         rho,
