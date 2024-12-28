0811520c1abe7d3182f85226b1c3f78fe7fcd219
DiegoRenner <diego.renner@hotmail.com>
2024-12-28 16:48:38 +0100

    refactor: separating run_inference_optax_4 demo into subfunctions

--- demos/run_inference_optax_4.py
+++ demos/run_inference_optax_4.py
@@ -65,215 +65,155 @@ CONFIG_FILENAME = "test/bifurcation/bifurcation.yml"
 # Set verbosity flag to control logging
 VERBOSE = True
 
-# Configure the simulation with the given configuration file
-(
-    N,
-    B,
-    J,
-    sim_dat,
-    sim_dat_aux,
-    sim_dat_const,
-    sim_dat_const_aux,
-    timepoints,
-    conv_tol,
-    CCFL,
-    input_data,
-    rho,
-    masks,
-    strides,
-    edges,
-    vessel_names,
-    cardiac_T,
-) = config_simulation(CONFIG_FILENAME, VERBOSE)
 
-UPPER = 1000
-
-
-# Set up and execute the simulation loop using JIT compilation
-SIM_LOOP_JIT = partial(jit, static_argnums=(0, 1, 12))(simulation_loop_unsafe)
-
-# Indices for selecting specific parts of the simulation data
-VESSEL_INDEX_1 = 1
-VAR_INDEX_1 = 4
-VESSEL_INDEX_2 = 2
-VAR_INDEX_2 = 5
-R1_1 = sim_dat_const_aux[VESSEL_INDEX_1, VAR_INDEX_1]
-R2_1 = sim_dat_const_aux[VESSEL_INDEX_1, VAR_INDEX_2]
-R1_2 = sim_dat_const_aux[VESSEL_INDEX_2, VAR_INDEX_1]
-R2_2 = sim_dat_const_aux[VESSEL_INDEX_2, VAR_INDEX_2]
-
-
-@compact
-def sim_loop_wrapper(params, upper=UPPER):
-    """
-    Wrapper function for running the simulation loop with a modified parameter value.
-
-    Args:
-        params (Array): Array containing scaling factor for the selected simulation constant.
-
-    Returns:
-        Array: Pressure values from the simulation with the modified parameter.
-    """
-    r1_1 = params[0] * R1_1 * 2
-    r2_1 = params[1] * R2_1 * 2
-    r1_2 = params[2] * R1_2 * 2
-    r2_2 = params[3] * R2_2 * 2
-    sim_dat_const_aux_new = jnp.array(sim_dat_const_aux)
-    sim_dat_const_aux_new = sim_dat_const_aux_new.at[VESSEL_INDEX_1, VAR_INDEX_1].set(
-        r1_1
-    )
-    sim_dat_const_aux_new = sim_dat_const_aux_new.at[VESSEL_INDEX_1, VAR_INDEX_2].set(
-        r2_1
-    )
-    sim_dat_const_aux_new = sim_dat_const_aux_new.at[VESSEL_INDEX_2, VAR_INDEX_1].set(
-        r1_2
-    )
-    sim_dat_const_aux_new = sim_dat_const_aux_new.at[VESSEL_INDEX_2, VAR_INDEX_2].set(
-        r2_2
-    )
-    sim_dat_wrapped, t_wrapped, p_wrapped = SIM_LOOP_JIT(  # pylint: disable=E1102
+def param_inf_optax(vessel_indices, var_indices, CONFIG_FILENAME):
+    # Configure the simulation with the given configuration file
+    (
         N,
         B,
+        J,
         sim_dat,
         sim_dat_aux,
         sim_dat_const,
-        sim_dat_const_aux_new,
+        sim_dat_const_aux,
+        timepoints,
+        conv_tol,
         CCFL,
         input_data,
         rho,
         masks,
         strides,
         edges,
-        upper=upper,
-    )
-    return sim_dat_wrapped, t_wrapped, p_wrapped
+        vessel_names,
+        cardiac_T,
+    ) = config_simulation(CONFIG_FILENAME, VERBOSE)
 
+    UPPER = 1000
 
-sim_dat_obs, t_obs, P_obs = sim_loop_wrapper([0.5, 0.5, 0.5, 0.5])
-sim_dat_obs_long, t_obs_long, P_obs_long = sim_loop_wrapper(
-    [0.5, 0.5, 0.5, 0.5], 120000
-)
+    # Set up and execute the simulation loop using JIT compilation
+    SIM_LOOP_JIT = partial(jit, static_argnums=(0, 1, 12))(simulation_loop_unsafe)
 
-# Define the folder to save the optimization results
-RESULTS_FOLDER = "results/inference_optax_4"
-if not os.path.isdir(RESULTS_FOLDER):
-    os.makedirs(RESULTS_FOLDER, mode=0o777)
+    # Indices for selecting specific parts of the simulation data
+    VESSEL_INDEX_1 = 1
+    VAR_INDEX_1 = 4
+    VESSEL_INDEX_2 = 2
+    VAR_INDEX_2 = 5
+    R1_1 = sim_dat_const_aux[VESSEL_INDEX_1, VAR_INDEX_1]
+    R2_1 = sim_dat_const_aux[VESSEL_INDEX_1, VAR_INDEX_2]
+    R1_2 = sim_dat_const_aux[VESSEL_INDEX_2, VAR_INDEX_1]
+    R2_2 = sim_dat_const_aux[VESSEL_INDEX_2, VAR_INDEX_2]
 
-
-class SimDense(Module):
-    features = 4
-    kernel_init: Callable[[jax.random.PRNGKey, tuple, jnp.dtype], jnp.ndarray] = (
-        uniform(1.0)
-    )
+    num_vessels = len(vessel_indices)
 
     @compact
-    def __call__(self) -> jnp.ndarray:
-        Rs = self.param(
-            "Rs",
-            self.kernel_init,
-            # lambda rng, shape: 0.5*jnp.ones(shape),
-            (4,),
+    def sim_loop_wrapper(params, upper=UPPER):
+        """
+        Wrapper function for running the simulation loop with a modified parameter value.
+
+        Args:
+            params (Array): Array containing scaling factor for the selected simulation constant.
+
+        Returns:
+            Array: Pressure values from the simulation with the modified parameter.
+        """
+        rs = params * sim_dat_const_aux[vessel_indices, var_indices] * 2
+        sim_dat_const_aux_new = jnp.array(sim_dat_const_aux)
+        sim_dat_const_aux_new = sim_dat_const_aux_new.at[
+            vessel_indices, var_indices
+        ].set(rs)
+        sim_dat_wrapped, t_wrapped, p_wrapped = SIM_LOOP_JIT(  # pylint: disable=E1102
+            N,
+            B,
+            sim_dat,
+            sim_dat_aux,
+            sim_dat_const,
+            sim_dat_const_aux_new,
+            CCFL,
+            input_data,
+            rho,
+            masks,
+            strides,
+            edges,
+            upper=upper,
+        )
+        return sim_dat_wrapped, t_wrapped, p_wrapped
+
+    sim_dat_obs, t_obs, P_obs = sim_loop_wrapper([0.5] * num_vessels)
+    sim_dat_obs_long, t_obs_long, P_obs_long = sim_loop_wrapper(
+        [0.5] * num_vessels, 120000
+    )
+
+    # Define the folder to save the optimization results
+    RESULTS_FOLDER = "results/inference_optax_4"
+    if not os.path.isdir(RESULTS_FOLDER):
+        os.makedirs(RESULTS_FOLDER, mode=0o777)
+
+    class SimDense(Module):
+        features = num_vessels
+        kernel_init: Callable[[jax.random.PRNGKey, tuple, jnp.dtype], jnp.ndarray] = (
+            uniform(1.0)
         )
 
-        _, _, y = sim_loop_wrapper(jax.nn.softplus(Rs))
-        return y
+        @compact
+        def __call__(self) -> jnp.ndarray:
+            Rs = self.param(
+                "Rs",
+                self.kernel_init,
+                # lambda rng, shape: 0.5*jnp.ones(shape),
+                (num_vessels,),
+            )
 
+            _, _, y = sim_loop_wrapper(jax.nn.softplus(Rs))
+            return y
 
-class Loss(object):
-    def __init__(self, axis=0, order=None):
-        super(Loss, self).__init__()
-        self.axis = axis
-        self.order = order
+    class Loss(object):
+        def __init__(self, axis=0, order=None):
+            super(Loss, self).__init__()
+            self.axis = axis
+            self.order = order
 
-    def relative_loss(self, s, s_pred):
-        return jnp.log(
-            jnp.mean(
-                jnp.power(
-                    jnp.linalg.norm(
-                        s_pred[:, [-1, -6]] - s[:, [-1, -6]], ord=None, axis=self.axis
-                    ),
-                    2,
-                )
-                / jnp.power(
-                    jnp.linalg.norm(s[:, [-1, -6]], ord=None, axis=self.axis), 2
+        def relative_loss(self, s, s_pred):
+            return jnp.log(
+                jnp.mean(
+                    jnp.power(
+                        jnp.linalg.norm(
+                            s_pred[:, [-1, -6]] - s[:, [-1, -6]],
+                            ord=None,
+                            axis=self.axis,
+                        ),
+                        2,
+                    )
+                    / jnp.power(
+                        jnp.linalg.norm(s[:, [-1, -6]], ord=None, axis=self.axis), 2
+                    )
                 )
             )
-        )
 
-    def __call__(self, s, s_pred):
-        return self.relative_loss(s, s_pred)
+        def __call__(self, s, s_pred):
+            return self.relative_loss(s, s_pred)
 
+    loss = Loss()
 
-loss = Loss()
+    def calculate_loss_train(state, params, batch):
+        s = batch
+        s_pred = state.apply_fn(params)
+        loss_value = loss(s, s_pred)
+        return loss_value
 
+    @jax.jit
+    def train_step(state, batch):
+        grad_fn = jax.value_and_grad(calculate_loss_train, argnums=1)
+        loss_value, grads = grad_fn(state, state.params, batch)
+        state = state.apply_gradients(grads=grads)
+        return state, loss_value
 
-def calculate_loss_train(state, params, batch):
-    s = batch
-    s_pred = state.apply_fn(params)
-    loss_value = loss(s, s_pred)
-    return loss_value
-
-
-@jax.jit
-def train_step(state, batch):
-    grad_fn = jax.value_and_grad(calculate_loss_train, argnums=1)
-    loss_value, grads = grad_fn(state, state.params, batch)
-    state = state.apply_gradients(grads=grads)
-    return state, loss_value
-
-
-def train_model(state, batch, num_epochs=None):
-    bar = tqdm.tqdm(np.arange(num_epochs))
-    params = jax.nn.softplus(state.params["params"]["Rs"])
-    plt.figure()
-    _, t, p = sim_loop_wrapper(params)
-    loss_val = loss(p, P_obs)
-    _, t, p = sim_loop_wrapper(params, upper=120000)
-    sorted_indices = np.argsort(t_obs_long[-12000:])
-    plt.plot(
-        t_obs_long[-12000:][sorted_indices],
-        P_obs_long[-12000:, -8][sorted_indices] / 133.322,
-        label="ground truth",
-        linewidth=0.5,
-    )
-    sorted_indices = np.argsort(t[-12000:])
-    plt.plot(
-        t[-12000:][sorted_indices],
-        p[-12000:, -8][sorted_indices] / 133.322,
-        label="learned",
-        linewidth=0.5,
-    )
-    lgnd = plt.legend(loc="upper right")
-    lgnd.legend_handles[0]._sizes = [30]
-    lgnd.legend_handles[1]._sizes = [30]
-    plt.xlabel("t/T")
-    plt.ylabel("P [mmHg]")
-    # plt.xlim([0.0, 1.0])
-    # plt.ylim([30, 140])
-    plt.tight_layout()
-    plt.savefig(f"{RESULTS_FOLDER}/{str(0)}_nt.pdf")
-    plt.savefig(f"{RESULTS_FOLDER}/{str(0)}_nt.png")
-    plt.savefig(f"{RESULTS_FOLDER}/{str(0)}_nt.jpeg")
-    plt.savefig(f"{RESULTS_FOLDER}/{str(0)}_nt.eps")
-    plt.title(
-        f"learning scaled Windkessel resistance parameters of a bifurcation:\n[R1_1, R2_1, R1_2, R2_2] =\n{params},\nloss = {loss_val}, \n wall-clock time = {0.0}"
-    )
-    plt.savefig(f"{RESULTS_FOLDER}/{str(0)}.pdf")
-    plt.savefig(f"{RESULTS_FOLDER}/{str(0)}.png")
-    plt.savefig(f"{RESULTS_FOLDER}/{str(0)}.jpeg")
-    plt.savefig(f"{RESULTS_FOLDER}/{str(0)}.eps")
-    plt.close()
-    total_time = 0
-    for epoch in bar:
-        starting_time = time.time_ns()
-        state, loss_val = train_step(state, batch)
-        total_time += time.time_ns() - starting_time
+    def train_model(state, batch, num_epochs=None):
+        bar = tqdm.tqdm(np.arange(num_epochs))
         params = jax.nn.softplus(state.params["params"]["Rs"])
-        bar.set_description(f"Loss: {loss_val}, Parameters {params}")
-        # if loss < 1e-6:
-        #    break
         plt.figure()
-        _, t, p = sim_loop_wrapper(params, 120000)
+        _, t, p = sim_loop_wrapper(params)
+        loss_val = loss(p, P_obs)
+        _, t, p = sim_loop_wrapper(params, upper=120000)
         sorted_indices = np.argsort(t_obs_long[-12000:])
         plt.plot(
             t_obs_long[-12000:][sorted_indices],
@@ -296,60 +236,124 @@ def train_model(state, batch, num_epochs=None):
         # plt.xlim([0.0, 1.0])
         # plt.ylim([30, 140])
         plt.tight_layout()
-        plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}_nt.pdf")
-        plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}_nt.png")
-        plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}_nt.jpeg")
-        plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}_nt.eps")
+        plt.savefig(f"{RESULTS_FOLDER}/{str(0)}_nt.pdf")
+        plt.savefig(f"{RESULTS_FOLDER}/{str(0)}_nt.png")
+        plt.savefig(f"{RESULTS_FOLDER}/{str(0)}_nt.jpeg")
+        plt.savefig(f"{RESULTS_FOLDER}/{str(0)}_nt.eps")
         plt.title(
-            f"learning scaled Windkessel resistance parameters of a bifurcation:\n[R1_1, R2_1, R1_2, R2_2] =\n{params},\nloss = {loss_val}, \n wall-clock time = {total_time / 1e9}, \n wall-clock time = {total_time / 1e9}"
+            f"learning scaled Windkessel resistance parameters of a bifurcation:\n[R1_1, R2_1, R1_2, R2_2] =\n{params},\nloss = {loss_val}, \n wall-clock time = {0.0}"
         )
-        plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}.pdf")
-        plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}.png")
-        plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}.jpeg")
-        plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}.eps")
+        plt.savefig(f"{RESULTS_FOLDER}/{str(0)}.pdf")
+        plt.savefig(f"{RESULTS_FOLDER}/{str(0)}.png")
+        plt.savefig(f"{RESULTS_FOLDER}/{str(0)}.jpeg")
+        plt.savefig(f"{RESULTS_FOLDER}/{str(0)}.eps")
         plt.close()
-    return state
+        total_time = 0
+        for epoch in bar:
+            starting_time = time.time_ns()
+            state, loss_val = train_step(state, batch)
+            total_time += time.time_ns() - starting_time
+            params = jax.nn.softplus(state.params["params"]["Rs"])
+            bar.set_description(f"Loss: {loss_val}, Parameters {params}")
+            # if loss < 1e-6:
+            #    break
+            plt.figure()
+            _, t, p = sim_loop_wrapper(params, 120000)
+            sorted_indices = np.argsort(t_obs_long[-12000:])
+            plt.plot(
+                t_obs_long[-12000:][sorted_indices],
+                P_obs_long[-12000:, -8][sorted_indices] / 133.322,
+                label="ground truth",
+                linewidth=0.5,
+            )
+            sorted_indices = np.argsort(t[-12000:])
+            plt.plot(
+                t[-12000:][sorted_indices],
+                p[-12000:, -8][sorted_indices] / 133.322,
+                label="learned",
+                linewidth=0.5,
+            )
+            lgnd = plt.legend(loc="upper right")
+            lgnd.legend_handles[0]._sizes = [30]
+            lgnd.legend_handles[1]._sizes = [30]
+            plt.xlabel("t/T")
+            plt.ylabel("P [mmHg]")
+            # plt.xlim([0.0, 1.0])
+            # plt.ylim([30, 140])
+            plt.tight_layout()
+            plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}_nt.pdf")
+            plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}_nt.png")
+            plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}_nt.jpeg")
+            plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}_nt.eps")
+            plt.title(
+                f"learning scaled Windkessel resistance parameters of a bifurcation:\n[R1_1, R2_1, R1_2, R2_2] =\n{params},\nloss = {loss_val}, \n wall-clock time = {total_time / 1e9}, \n wall-clock time = {total_time / 1e9}"
+            )
+            plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}.pdf")
+            plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}.png")
+            plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}.jpeg")
+            plt.savefig(f"{RESULTS_FOLDER}/{str(epoch + 1)}.eps")
+            plt.close()
+        return state
+
+    print("Model Initialized")
+    lr = 1e-2
+    transition_steps = 10
+    decay_rate = 0.9
+    weight_decay = 0
+    seed = 0
+    epochs = 5000
+
+    model = SimDense()
+
+    params = model.init(random.key(21234))
+
+    print("Initial Parameters: ", jax.nn.softplus(params["params"]["Rs"]))
+
+    exponential_decay_scheduler = optax.exponential_decay(
+        init_value=lr,
+        transition_steps=transition_steps,
+        decay_rate=decay_rate,
+        transition_begin=0,
+        staircase=False,
+    )
+
+    # optimizer = optax.adamw(learning_rate=lr, weight_decay=weight_decay)
+    optimizer = optax.adafactor(lr)
+
+    model_state = train_state.TrainState.create(
+        apply_fn=model.apply, params=params, tx=optimizer
+    )
+
+    trained_model_state = train_model(model_state, P_obs, num_epochs=epochs)
+
+    y = model_state.apply_fn(trained_model_state.params)[0]
+
+    print(
+        f"Final Loss: {loss(P_obs, y)} and Parameters: {jax.nn.softplus(trained_model_state.params["params"]["Rs"])}"
+    )
+
+    plt.figure()
+    plt.plot(t_obs, P_obs, "b-", label="Baseline")
+    plt.plot(t_obs, y, "r--", label="Predicted")
+    plt.legend()
+    plt.show()
 
 
-print("Model Initialized")
-lr = 1e-2
-transition_steps = 10
-decay_rate = 0.9
-weight_decay = 0
-seed = 0
-epochs = 5000
+def main():
+    """
+    Main function for running the parameter inference using NumPyro.
+    """
+    # Define the indices of the vessels and variables to select for inference
+    VESSEL_INDICES = [1, 1]
+    VAR_INDICES = [4, 5]
 
-model = SimDense()
+    # Define the configuration file for the simulation
+    CONFIG_FILENAME = "test/bifurcation/bifurcation.yml"
 
-params = model.init(random.key(21234))
+    # Run the parameter inference using NumPyro
+    param_inf_optax(VESSEL_INDICES, VAR_INDICES, CONFIG_FILENAME)
+    print("Inference complete.")
 
-print("Initial Parameters: ", jax.nn.softplus(params["params"]["Rs"]))
 
-exponential_decay_scheduler = optax.exponential_decay(
-    init_value=lr,
-    transition_steps=transition_steps,
-    decay_rate=decay_rate,
-    transition_begin=0,
-    staircase=False,
-)
-
-# optimizer = optax.adamw(learning_rate=lr, weight_decay=weight_decay)
-optimizer = optax.adafactor(lr)
-
-model_state = train_state.TrainState.create(
-    apply_fn=model.apply, params=params, tx=optimizer
-)
-
-trained_model_state = train_model(model_state, P_obs, num_epochs=epochs)
-
-y = model_state.apply_fn(trained_model_state.params)[0]
-
-print(
-    f"Final Loss: {loss(P_obs, y)} and Parameters: {jax.nn.softplus(trained_model_state.params["params"]["Rs"])}"
-)
-
-plt.figure()
-plt.plot(t_obs, P_obs, "b-", label="Baseline")
-plt.plot(t_obs, y, "r--", label="Predicted")
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    main()
