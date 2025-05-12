ed697e2b257c0971860c11678421347e26cb89a0
Diego Renner <diego.renner@hotmail.com>
2025-05-12 02:53:21 +0200

    feat: moved TypeAlias declarations to separate file

--- src/model.py
+++ src/model.py
@@ -34,43 +34,41 @@ from src.initialise import (
 )
 from src.IOutils import save_temp_data
 from src.solver import compute_dt, solve_model
-from typing import TypeAlias, Annotated, Unpack, Literal
-from numpy.typing import NDArray
+from src.types import (
+    PressureReturn,
+    StaticScalarInt,
+    SimDat,
+    SimDatAux,
+    SimDatConst,
+    SimDatConstAux,
+    ScalarFloat,
+    Strides,
+    Edges,
+    Masks,
+    Timepoints,
+    StaticSimDat,
+    StaticSimDatAux,
+    StaticSimDatConst,
+    StaticSimDatConstAux,
+    StaticTimepoints,
+    StaticScalarFloat,
+    StaticInputData,
+    StaticMasks,
+    StaticStrides,
+    StaticEdges,
+    String,
+    Strings,
+    Bool,
+    InputData,
+    TimepointsReturn,
+)
+
 
 numpyro.set_platform("cpu")
 # os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=8' # Use 8 CPU devices
 # os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=32' # Use 32 CPU devices
 # jax.devices("cpu")[0]
 # print(jax.local_device_count())
-
-SimDat: TypeAlias = Float[Array, "5 K"]  # type: ignore
-StaticSimDat: TypeAlias = np.ndarray
-SimDatAux: TypeAlias = Float[Array, "N 3"]  # type: ignore
-StaticSimDatAux: TypeAlias = np.ndarray
-SimDatConst: TypeAlias = Float[Array, "7 K"]  # type: ignore
-StaticSimDatConst: TypeAlias = np.ndarray
-SimDatConstAux: TypeAlias = Float[Array, "N 7"]  # type: ignore
-StaticSimDatConstAux: TypeAlias = np.ndarray
-Timepoints: TypeAlias = Float[Array, " J"]  # type: ignore
-StaticTimepoints: TypeAlias = np.ndarray
-ScalarFloat: TypeAlias = Float[Array, ""]  # type: ignore
-StaticScalarFloat: TypeAlias = float
-StaticScalarInt: TypeAlias = int
-InputData: TypeAlias = Float[Array, "2*N H"]  # type: ignore
-StaticInputData: TypeAlias = np.ndarray
-Masks: TypeAlias = Integer[Array, "2 K"]  # type: ignore
-StaticMasks: TypeAlias = np.ndarray
-Strides: TypeAlias = Integer[Array, "N 5"]  # type: ignore
-StaticStrides: TypeAlias = np.ndarray
-Edges: TypeAlias = Integer[Array, "N 10"]  # type: ignore
-StaticEdges: TypeAlias = np.ndarray
-TimepointsReturn: TypeAlias = Float[Array, " I"]  # type: ignore
-StaticTimepointsReturn: TypeAlias = np.ndarray
-PressureReturn: TypeAlias = Float[Array, "I 5*N"]  # type: ignore
-StaticPressureReturn: TypeAlias = np.ndarray
-String: TypeAlias = str
-Strings: TypeAlias = list[str]
-Bool: TypeAlias = bool
 
 
 @jaxtyped(typechecker=typechecker)
---
+++ src/types.py
@@ -0,0 +1,33 @@
+import numpy as np
+from jaxtyping import Array, Float, Integer, jaxtyped
+from typing import TypeAlias, Annotated, Unpack, Literal
+from numpy.typing import NDArray
+
+SimDat: TypeAlias = Float[Array, "5 K"]  # type: ignore
+StaticSimDat: TypeAlias = np.ndarray
+SimDatAux: TypeAlias = Float[Array, "N 3"]  # type: ignore
+StaticSimDatAux: TypeAlias = np.ndarray
+SimDatConst: TypeAlias = Float[Array, "7 K"]  # type: ignore
+StaticSimDatConst: TypeAlias = np.ndarray
+SimDatConstAux: TypeAlias = Float[Array, "N 7"]  # type: ignore
+StaticSimDatConstAux: TypeAlias = np.ndarray
+Timepoints: TypeAlias = Float[Array, " J"]  # type: ignore
+StaticTimepoints: TypeAlias = np.ndarray
+ScalarFloat: TypeAlias = Float[Array, ""]  # type: ignore
+StaticScalarFloat: TypeAlias = float
+StaticScalarInt: TypeAlias = int
+InputData: TypeAlias = Float[Array, "2*N H"]  # type: ignore
+StaticInputData: TypeAlias = np.ndarray
+Masks: TypeAlias = Integer[Array, "2 K"]  # type: ignore
+StaticMasks: TypeAlias = np.ndarray
+Strides: TypeAlias = Integer[Array, "N 5"]  # type: ignore
+StaticStrides: TypeAlias = np.ndarray
+Edges: TypeAlias = Integer[Array, "N 10"]  # type: ignore
+StaticEdges: TypeAlias = np.ndarray
+TimepointsReturn: TypeAlias = Float[Array, " I"]  # type: ignore
+StaticTimepointsReturn: TypeAlias = np.ndarray
+PressureReturn: TypeAlias = Float[Array, "I 5*N"]  # type: ignore
+StaticPressureReturn: TypeAlias = np.ndarray
+String: TypeAlias = str
+Strings: TypeAlias = list[str]
+Bool: TypeAlias = bool
